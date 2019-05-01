c772fbe4e64ea77abc0588716fb3cc199d4fa75a
Martin Caldarola <m.caldarola@tudelft.nl>
2019-05-01 11:52:05 +0200

    changed the name Motors for motor in the instrument folder

--- hyperion/controller/thorlabs/TDC001.py
+++ hyperion/controller/thorlabs/TDC001.py
@@ -24,7 +24,7 @@ Since this package is based on APT.dll it only works on Windows. For Linux and M
 
 **List of reported working devices**
 
-I have tested thorlabs_apt only with the K10CR1 rotation stage, but it should work with all motors supported by APT. If it works with other motors as well, please let me know and I will add it here. Otherwise file a bug report or fix the problem yourself and open a pull request.
+I have tested thorlabs_apt only with the K10CR1 rotation stage, but it should work with all motor supported by APT. If it works with other motor as well, please let me know and I will add it here. Otherwise file a bug report or fix the problem yourself and open a pull request.
 
 - BSC101
     - NRT150
@@ -136,7 +136,7 @@ import platform
 
 class TDC001(BaseController):
     """
-    Class TDC001 to control the DC stepper motors from Thorlabs
+    Class TDC001 to control the DC stepper motor from Thorlabs
     """
     
     _lib = None
@@ -479,7 +479,7 @@ class TDC001(BaseController):
     @property
     def active_channel(self):
         """
-        Active channel number. Used with motors having more than 1 channel.
+        Active channel number. Used with motor having more than 1 channel.
 
         CHAN1_INDEX = 0 : channel 1
         CHAN2_INDEX = 1 : channel 2
@@ -706,7 +706,7 @@ class TDC001(BaseController):
 
     def set_motor_parameters(self, steps_per_rev, gear_box_ratio):
         """
-        Sets motor parameters. Note that this is not possible with all motors,
+        Sets motor parameters. Note that this is not possible with all motor,
         see documentation from Thorlabs.
 
         Parameters
