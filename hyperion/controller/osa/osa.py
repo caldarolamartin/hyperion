3f783bf82cce3edb7eabfde2f0b0e7f7e82c27c1
Irina Komen <i.komen@tudelft.nl>
2019-07-01 11:02:41 +0200

    initialise osa controller

---
+++ hyperion/controller/osa/osa.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+    ===============
+    osa controller
+    ===============
+
+    This controller (osa.py) supplies one class with several methods to communicate
+    with the osa machine from ando AQ6317B model: 1MODD18012_0074
+
+
+
+
+"""
