62f6620b8f41f7135ba00df8bbd7d06c408dc467
Irina Komen <i.komen@tudelft.nl>
2019-07-17 11:31:49 +0200

    documentation has been added to the osacontroller class

--- hyperion/controller/osa/osacontroller.py
+++ hyperion/controller/osa/osacontroller.py
@@ -15,10 +15,7 @@ import time
 from hyperion.controller.base_controller import BaseController
 
 class OsaController(BaseController):
-    """ Example output device that does not connect to anything"""
-
-    FAKE_RESPONSES = {'A': 1,
-                      }
+    """ The controller for the Osa machine."""
 
     def __init__(self, settings={'port': 'AUTO', 'dummy': False}):
         """ Init of the class.
@@ -48,8 +45,6 @@ class OsaController(BaseController):
         self._sensitivities = ['high1', 'high2', 'high3', 'norm_hold', 'norm_auto', 'mid']
         self._time_constants = [0.02, 0.12, 1.125, 0.001, 0.001, 0.002]
         self._sens_commands = ['SH1', 'SH2', 'SH3', 'SNHD', 'SNAT', 'SMID']
-        #self.sensitivities = [['high1',1],['high2',2],['high3',3],['norm_hold',4],['norm_auto',5],['mid',6]]
-        #self.sensitivities = {'norm_auto':5, 'norm_hold':4, 'mid':6, 'high1':1, 'high2':2, 'high3':3 }
 
     def initialize(self):
         """ Starts the connection to the device in port """
@@ -73,13 +68,18 @@ class OsaController(BaseController):
         # this is to prevent you to close the device connection if you
         # have not initialized it inside a with statement
         #self._amplitude = self.query('A?')
-        self.start_wav      # make sure self._start_wav is the same as on machine
-        self.end_wav       # make sure ...
-        self.sample_points  # make sure ...
-        self.sensitivity    # make sure ...
-        self.optical_resolution
+        self.start_wav; self.end_wav; self.sample_points; self.sensitivity; self.optical_resolution
+        # make sure self._start_wav/end_wav/sample_points/optical_resolution is the same as on the osa machine.
+        #This is mandatory
 
     def wait_for_osa(self, timeout=None):
+        """
+        Method to let the program do nothing for a while
+        in order to create enough time to let the osa machine take a spectrum.
+        If timeout is not specified a timeout will be calculated and used.
+        :param timeout: time in seconds how long the program must wait before it resumes
+        :return: -
+        """
         if timeout==None:
             timeout = 4.0 + 1.05 * self._sample_points * self._time_constants[self._sensitivity-1]
         start_time = time.time()
@@ -92,11 +92,20 @@ class OsaController(BaseController):
 
     @property
     def sensitivity(self):
+        """
+        Get the sensitivity from the osa machine
+        :return:
+        """
         self._sensitivity = int(self._osa.query_ascii_values('SENS?')[0])
         return self._sensitivities[self._sensitivity-1]
 
     @sensitivity.setter
     def sensitivity(self, sensitivity_string):
+        """
+        Set the sensitivity for the osa machine.
+        :param sensitivity_string: a string with what sensitivity should be used.
+        :return: -
+        """
         if sensitivity_string in self._sensitivities:
             sensitivity_number = self._sensitivities.index(sensitivity_string)
         else:
@@ -108,19 +117,23 @@ class OsaController(BaseController):
             if sensitivity_number != self._sensitivity:
                 self.logger.warning('Value not set in OSA')
 
-            #print('STAWL{:1.2f}'.format(self._start_wav))
-            #self._osa.write('STAWL{:1.2f}'.format(self._start_wav))
-
-
     @property
     def start_wav(self):
+        """
+        Get the start wavelength
+        :return:
+        """
         self._start_wav = self._osa.query_ascii_values('STAWL?')[0]
         return self._start_wav
 
 
     @start_wav.setter
     def start_wav(self, start_wav):
-        # note that OSA works from 600 to 1750 nm
+        """
+        Set the start wavelength
+        :param start_wav: a pint quantity
+        :return:
+        """
         start_wav = round(start_wav, 2)
         if start_wav < 600 or start_wav > 1750:
             self.logger.warning('Invalid start_wav value')
@@ -131,16 +144,22 @@ class OsaController(BaseController):
             if start_wav != self._start_wav:
                 self.logger.warning('Start_wav value not set in OSA')
 
-            #print('STAWL{:1.2f}'.format(self._start_wav))
-            #self._osa.write('STAWL{:1.2f}'.format(self._start_wav))
-
     @property
     def end_wav(self):
+        """
+        Get the end wavelength
+        :return:
+        """
         self._end_wav = self._osa.query_ascii_values('STPWL?')[0]
         return self._end_wav
 
     @end_wav.setter
     def end_wav(self, end_wav):
+        """
+        Set the end wavelength
+        :param end_wav: a float
+        :return:
+        """
         end_wav = round(end_wav, 2)
         if end_wav <600 or end_wav > 1750:
             self.logger.warning("invalid end_wav value")
@@ -150,16 +169,23 @@ class OsaController(BaseController):
             self._end_wav = self._osa.query_ascii_values('STPWL?')[0]
             if end_wav != self._end_wav:
                 self.logger.warning("End_wav value not set in OSA")
-            #print('STpWL{:1.2f}'.format(self._end_wav))
-            #self._end_wav = end_wav
 
     @property
     def optical_resolution(self):
+        """
+        Get the optical resolution
+        :return:
+        """
         self._optical_resolution = self._osa.query_ascii_values('RESLN?')[0]
         return self._optical_resolution
 
     @optical_resolution.setter
     def optical_resolution(self, optical_resolution):
+        """
+        Set the optical resolution
+        :param optical_resolution:
+        :return:
+        """
         optical_resolution = round(optical_resolution, 2)
         if not optical_resolution in [0.01,0.02,0.05,0.1,0.2,0.5,1.0,2.0,5.0]:
             self.logger.warning("invalid optical resolution value")
@@ -173,11 +199,21 @@ class OsaController(BaseController):
 
     @property
     def sample_points(self):
+        """
+        Get the sample points
+        :return:
+        """
         self._sample_points = self._osa.query_ascii_values('SMPL?')[0]
         return self._sample_points
 
     @sample_points.setter
     def sample_points(self, sample_points):
+        """
+        Set the sample points
+        :param sample_points: an int. The recommended sample points are:
+        1+(2 *(end_wav-start_wav)/ optical_resolution)
+        :return:
+        """
         if sample_points != self._sample_points:
             self._sample_points = sample_points
             self._osa.write('SMPL{:1.2f}'.format(sample_points))
@@ -186,11 +222,19 @@ class OsaController(BaseController):
                 self.logger.warning("The sample points value did not set in OSA")
 
     def perform_single_sweep(self):
+        """
+        Gives a command to the osa machine to
+        do a single sweep.
+        :return:
+        """
         self._osa.write('SGL')
 
     def get_data(self):
-        # wait for OSA to finish before grabbing data
-
+        """
+        Calculates the data created with the single sweep.
+        Wait for OSA to finish before grabbing data
+        :return:
+        """
         wav = self._osa.query_ascii_values('WDATA')[1:]
         spec = self._osa.query_ascii_values('LDATA')[1:]
 
@@ -233,7 +277,7 @@ class OsaController(BaseController):
         :return: fake result
         :rtype: string
         """
-        return self.FAKE_RESPONSES['A']
+        return 'A'
 
     def write(self, msg):
         """ Writes into the device
@@ -247,7 +291,7 @@ class OsaController(BaseController):
         """
         in this method the parameters for the osa machine are set with
         hand in order to quickly get results.
-        :param dev: the osa device object.
+        :param self: the osa device object.
         :return: -
         """
         # start and end between 600.00 and 1750.00
@@ -255,19 +299,19 @@ class OsaController(BaseController):
         self.end_wav = 1200.00
         # allowed are 0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0
         self.optical_resolution = 1.00
+        #recommended sample points are: 1+(2 *(end_wav-start_wav)/ optical_resolution)
         self.sample_points = 601.00
 
 
 class OsaControllerDummy(OsaController):
     """
-    Example Controller Dummy
+    Example Controller Dummy for the Osa machine
     ========================
 
     A dummy version of the Example Controller.
 
     In essence we have the same methods and we re-write the query to answer something meaningful but
     without connecting to the real device.
-
     """
 
     def query(self, msg):
@@ -292,9 +336,6 @@ if __name__ == "__main__":
                                                                  backupCount=_logger_settings['backupCount']),
                             logging.StreamHandler()])
 
-
-
-
     dummy = False  # change this to false to work with the real device in the COM specified below.
 
     if dummy:
@@ -304,7 +345,7 @@ if __name__ == "__main__":
 
     with my_class(settings={'dummy': dummy}) as dev:
         dev.initialize()
-
+        #change paramaters
         print('OSA start wav is {}'.format(dev._start_wav))
         dev.start_wav = 800.00
         print('OSA start wav is {}'.format(dev._start_wav))
@@ -328,7 +369,7 @@ if __name__ == "__main__":
         #dev.set_settings_for_osa()
 
         dev.perform_single_sweep()
-        dev.wait_for_osa(5)
+        dev.wait_for_osa()
         #dev.get_data()
         dev.finalize()
 
