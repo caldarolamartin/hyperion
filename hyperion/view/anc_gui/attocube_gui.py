5a77e76db843ed4bbe54268e63a1de764c5729d4
Irina Komen <i.komen@tudelft.nl>
2019-08-28 11:13:21 +0200

    Added a new textfield where the amount of steps can be given and changed some names in the code

--- hyperion/view/anc_gui/attocube_gui.py
+++ hyperion/view/anc_gui/attocube_gui.py
@@ -39,21 +39,23 @@ class App(QWidget):
         self.make_move_to_absolute_position_label()
         self.make_move_to_relative_position_label()
         self.make_piezo_scanner_position_label()
-        self.make_single_step_left_label()
-        self.make_single_step_right_label()
+        self.make_single_step_forward_label()
+        self.make_single_step_backward_label()
         self.make_move_scanner_label()
         self.make_actual_position_label()
+        self.make_step_amount_label()
     def make_textfields(self):
         self.make_move_to_absolute_position_textfield()
         self.make_move_to_relative_position_textfield()
         self.make_move_scanner_textfield()
         self.make_amplitude_textfield()
         self.make_frequency_textfield()
+        self.make_step_amount_textfield()
     def make_buttons(self):
         self.make_move_to_absolute_position_button()
         self.make_move_to_relative_position_button()
-        self.make_step_position_left_button()
-        self.make_step_position_right_button()
+        self.make_step_position_forward_button()
+        self.make_step_position_backward_button()
         self.make_move_scanner_button()
         self.make_move_scanner_button()
         self.make_frequency_button()
@@ -73,22 +75,26 @@ class App(QWidget):
         self.move_to_relative_position_label = QLabel(self)
         self.move_to_relative_position_label.setText("Move to\nrelative position in nm: ")
         self.grid_layout.addWidget(self.move_to_relative_position_label, 2, 0)
-    def make_single_step_left_label(self):
-        self.pos_single_step_left_label = QLabel(self)
-        self.pos_single_step_left_label.setText("Move single step\nleft: ")
-        self.grid_layout.addWidget(self.pos_single_step_left_label, 3, 0)
-    def make_single_step_right_label(self):
-        self.pos_single_step_right_label = QLabel(self)
-        self.pos_single_step_right_label.setText("Move single step\nright: ")
-        self.grid_layout.addWidget(self.pos_single_step_right_label, 4, 0)
+    def make_single_step_forward_label(self):
+        self.pos_single_step_forward_label = QLabel(self)
+        self.pos_single_step_forward_label.setText("Move single step\nleft: ")
+        self.grid_layout.addWidget(self.pos_single_step_forward_label, 3, 0)
+    def make_single_step_backward_label(self):
+        self.pos_single_step_backward_label = QLabel(self)
+        self.pos_single_step_backward_label.setText("Move single step\nright: ")
+        self.grid_layout.addWidget(self.pos_single_step_backward_label, 4, 0)
     def make_move_scanner_label(self):
         self.move_scanner_label = QLabel(self)
         self.move_scanner_label.setText("Move scanner in nm: ")
-        self.grid_layout.addWidget(self.move_scanner_label, 5, 0)
+        self.grid_layout.addWidget(self.move_scanner_label, 5, 2)
     def make_actual_position_label(self):
         self.actual_position_label = QLabel(self)
         self.actual_position_label.setText("currently\nunavailable")
         self.grid_layout.addWidget(self.actual_position_label, 0, 1)
+    def make_step_amount_label(self):
+        self.step_amount_label = QLabel(self)
+        self.step_amount_label.setText("amount of steps:")
+        self.grid_layout.addWidget(self.step_amount_label, 0, 5)
 
     def make_move_to_absolute_position_textfield(self):
         self.move_to_absolute_position_textfield = QLineEdit(self)
@@ -101,7 +107,7 @@ class App(QWidget):
     def make_move_scanner_textfield(self):
         self.move_scanner_textfield = QLineEdit(self)
         self.move_scanner_textfield.setText("0")
-        self.grid_layout.addWidget(self.move_scanner_textfield, 5, 1)
+        self.grid_layout.addWidget(self.move_scanner_textfield, 5, 3)
     def make_amplitude_textfield(self):
         self.amplitude_textfield = QLineEdit(self)
         self.amplitude_textfield.setText("0")
@@ -110,8 +116,11 @@ class App(QWidget):
         self.frequency_textfield = QLineEdit(self)
         self.frequency_textfield.setText("0")
         self.grid_layout.addWidget(self.frequency_textfield, 1, 4)
+    def make_step_amount_textfield(self):
+        self.step_amount_textfield = QLineEdit(self)
+        self.step_amount_textfield.setText("1")
+        self.grid_layout.addWidget(self.step_amount_textfield, 5, 1)
 
-        #buttons
     def make_move_to_absolute_position_button(self):
         self.move_to_absolute_position_button = QPushButton("absolute position", self)
         self.move_to_absolute_position_button.setToolTip("move to the absolute position")
@@ -122,21 +131,21 @@ class App(QWidget):
         self.move_to_relative_position_button.setToolTip("move to the relative position")
         self.move_to_relative_position_button.clicked.connect(self.move_relative_position)
         self.grid_layout.addWidget(self.move_to_relative_position_button, 2, 2)
-    def make_step_position_left_button(self):
-        self.step_position_left_button = QPushButton("step left", self)
-        self.step_position_left_button.setToolTip("move a single step to the left")
-        self.step_position_left_button.clicked.connect(self.go_single_step_left)
-        self.grid_layout.addWidget(self.step_position_left_button, 3, 1)
-    def make_step_position_right_button(self):
-        self.step_position_right_button = QPushButton("step right", self)
-        self.step_position_right_button.setToolTip("move a single step to the right")
-        self.step_position_right_button.clicked.connect(self.go_single_step_right)
-        self.grid_layout.addWidget(self.step_position_right_button, 4, 1)
+    def make_step_position_forward_button(self):
+        self.step_position_forward_button = QPushButton("step left", self)
+        self.step_position_forward_button.setToolTip("move a single step to the left")
+        self.step_position_forward_button.clicked.connect(self.go_single_step_forward)
+        self.grid_layout.addWidget(self.step_position_forward_button, 3, 1)
+    def make_step_position_backward_button(self):
+        self.step_position_backward_button = QPushButton("step right", self)
+        self.step_position_backward_button.setToolTip("move a single step to the right")
+        self.step_position_backward_button.clicked.connect(self.go_single_step_backward)
+        self.grid_layout.addWidget(self.step_position_backward_button, 4, 1)
     def make_move_scanner_button(self):
         self.move_scanner_button = QPushButton("move scanner", self)
         self.move_scanner_button.setToolTip("Move the scanner as specified in the textfield(which you do, not I)")
         self.move_scanner_button.clicked.connect(self.move_scanner)
-        self.grid_layout.addWidget(self.move_scanner_button, 5, 2)
+        self.grid_layout.addWidget(self.move_scanner_button, 5, 4)
     def make_amplitude_button(self):
         self.amplitude_button = QPushButton("set the amplitude", self)
         self.amplitude_button.setToolTip("Sets the amplitude by clicking it\nbetween 0 and 60 V")
@@ -159,13 +168,11 @@ class App(QWidget):
     def update_gui(self):
         self.update_actual_position_label()
         self.enable_or_disable_scanner_piezo_widgets()
-
     def update_actual_position_label(self):
         try:
             self.actual_position_label.setText("Position in nm: "+str(self.anc350_instrument.controller.getPosition(self.anc350_instrument.attocube_piezo_dict[self.scanner_piezo_combobox.currentText()])))
         except Exception:
             self.actual_position_label.setText("currently\nunavailable")
-
     def enable_or_disable_scanner_piezo_widgets(self):
         #make sure that if the scanner stuff is enabled that the piezo stuff is disabled and viceversa
         if "Stepper" in self.scanner_piezo_combobox.currentText():
@@ -177,8 +184,8 @@ class App(QWidget):
             self.move_to_absolute_position_button.setEnabled(True)
             self.move_to_relative_position_textfield.setEnabled(True)
             self.move_to_relative_position_button.setEnabled(True)
-            self.step_position_left_button.setEnabled(True)
-            self.step_position_right_button.setEnabled(True)
+            self.step_position_forward_button.setEnabled(True)
+            self.step_position_backward_button.setEnabled(True)
             self.amplitude_textfield.setEnabled(True)
             self.amplitude_button.setEnabled(True)
             self.frequency_textfield.setEnabled(True)
@@ -189,8 +196,8 @@ class App(QWidget):
             self.move_to_absolute_position_button.setEnabled(False)
             self.move_to_relative_position_textfield.setEnabled(False)
             self.move_to_relative_position_button.setEnabled(False)
-            self.step_position_left_button.setEnabled(False)
-            self.step_position_right_button.setEnabled(False)
+            self.step_position_forward_button.setEnabled(False)
+            self.step_position_backward_button.setEnabled(False)
             self.amplitude_textfield.setEnabled(False)
             self.amplitude_button.setEnabled(False)
             self.frequency_textfield.setEnabled(False)
@@ -214,10 +221,18 @@ class App(QWidget):
         step = int(self.move_to_relative_position_textfield.text())* ur('nm') #step: amount to move in nm, can be both positive and negative
         self.anc350_instrument.move_relative(axis, step)
         self.update_actual_position_label()
-    def go_single_step_left(self):
+    def go_single_step_forward(self):
         print("go a single step to the left")
-    def go_single_step_right(self):
+        axis = self.scanner_piezo_combobox.currentText()  # the current stepper
+        direction = 0 # forward = 0
+        amount = int(self.step_amount_textfield.text())
+        self.anc350_instrument.given_step(axis, direction, amount)
+    def go_single_step_backward(self):
         print("go a single step to the right")
+        axis = self.scanner_piezo_combobox.currentText()  # the current stepper
+        direction = 1 # backward = 1
+        amount = int(self.step_amount_textfield.text())
+        self.anc350_instrument.given_step(axis, direction, amount)
     def move_scanner(self):
         print("move the scanner by a...value")
     def set_amplitude(self):
