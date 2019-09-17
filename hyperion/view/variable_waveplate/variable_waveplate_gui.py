1dbe57fd342f0dd7698b6d9397559a198bc9047c
Martin Caldarola <m.caldarola@tudelft.nl>
2019-09-17 13:44:56 +0200

    apparently it works with dummy.

--- hyperion/view/variable_waveplate/variable_waveplate_gui.py
+++ hyperion/view/variable_waveplate/variable_waveplate_gui.py
@@ -15,7 +15,7 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QStandardItem, QColor
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout, QComboBox, QLabel, QLineEdit
 from hyperion.instrument.variable_waveplate.variable_waveplate import VariableWaveplate
-from hyperion import Q_
+from hyperion import Q_, ur
 
 #todo checkout if the device is on the computer if this class can work with the variablewaveplate/lcc25
 
@@ -30,13 +30,17 @@ class VariableWaveplateGui(QWidget):
         :type an instance of the variable_waveplate instrument
         """
         super().__init__()
-        self.title = 'variable waveplate gui'
+        self.logger = logging.getLogger(__name__)
+        self._wavelength = 532*ur('nm')
+        self.logger.debug('Default wavelength: {}'.format(self._wavelength))
+        self.title = 'LCC25 variable waveplate instrument (GUI)'
         self.left = 40
         self.top = 40
         self.width = 320
         self.height = 200
         self.variable_waveplate_ins = variable_waveplate_ins
         self.initUI()
+
     def set_gui_specifics(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
@@ -44,17 +48,14 @@ class VariableWaveplateGui(QWidget):
         self.setLayout(self.grid_layout)
 
     def initUI(self):
+        """ Initializes the gui elements """
         self.set_gui_specifics()
-
         self.set_elements_in_gui()
-
         self.show()
 
     def set_elements_in_gui(self):
         self.set_labels()
-
         self.set_textfields()
-
         self.set_miscelanious_gui_stuff()
 
     def set_labels(self):
@@ -64,35 +65,43 @@ class VariableWaveplateGui(QWidget):
         self.set_quater_waveplate_label()
         self.set_frequency_label()
         self.set_output_label()
+
     def set_textfields(self):
         self.set_voltage_1_textfield()
         self.set_voltage_2_textfield()
         self.set_quater_waveplate_textfield()
         self.set_frequency_textfield()
+
     def set_miscelanious_gui_stuff(self):
         self.set_mode_combobox()
         self.set_submit_button()
         self.set_output_dropdown()
+
     def set_frequency_label(self):
         self.frequency_label = QLabel(self)
-        self.frequency_label.setText("freq in Hz:")
+        self.frequency_label.setText("Freq in Hz:")
         self.grid_layout.addWidget(self.frequency_label, 0, 2)
+
     def set_quater_waveplate_label(self):
         self.quater_waveplate_label = QLabel(self)
-        self.quater_waveplate_label.setText("qw in Hz:")
+        self.quater_waveplate_label.setText("Wavelength for Quarter-wave plate:")
         self.grid_layout.addWidget(self.quater_waveplate_label, 3, 0)
+
     def set_voltage_2_label(self):
         self.voltage_2_label = QLabel(self)
-        self.voltage_2_label.setText("V 2, from 0 to 25 V:")
+        self.voltage_2_label.setText("V2, in (0, 25) V:")
         self.grid_layout.addWidget(self.voltage_2_label, 2, 0)
+
     def set_voltage_1_label(self):
         self.voltage_1_label = QLabel(self)
-        self.voltage_1_label.setText("V 1, from 0 to 25 V:")
+        self.voltage_1_label.setText("V1, in (0,25) V:")
         self.grid_layout.addWidget(self.voltage_1_label, 1, 0)
+
     def set_mode_label(self):
         self.mode_label = QLabel(self)
         self.mode_label.setText("mode:")
         self.grid_layout.addWidget(self.mode_label, 0, 0)
+
     def set_output_label(self):
         self.output_label = QLabel(self)
         self.output_label.setText("output:")
@@ -105,7 +114,7 @@ class VariableWaveplateGui(QWidget):
 
     def set_quater_waveplate_textfield(self):
         self.quater_waveplate_textfield = QLineEdit(self)
-        self.quater_waveplate_textfield.setText("Idk, ask Martin")
+        self.quater_waveplate_textfield.setText(str(self._wavelength))
         self.grid_layout.addWidget(self.quater_waveplate_textfield, 3, 1)
 
     def set_voltage_1_textfield(self):
@@ -128,8 +137,9 @@ class VariableWaveplateGui(QWidget):
         """
         With the combobox all the different modes are shown.
         """
+        self.logger.debug('Setting combobox')
         self.mode_combobox = QComboBox(self)
-        self.mode_combobox.addItems(["Voltage1", "Voltage2", "Modulation"])
+        self.mode_combobox.addItems(["Voltage1", "Voltage2", "Modulation", "QWP"])
         #making sure that the user can only enter values in the right textbox when
         #changes are made to the mode it is in.
         self.voltage_2_textfield.setReadOnly(True)
@@ -174,13 +184,13 @@ class VariableWaveplateGui(QWidget):
 
     def submit_button_clicked(self):
         """
-        Get the paramters from the gui and sent these to the
+        Get the parameters from the gui and sent these to the
         instrument of the variable waveplate.
-        :return:
+
+
         """
-
+        self.logger.debug('Submit button was clicked...')
         self.set_output_mode()
-
 
         if self.get_mode() == "Voltage1":
             self.variable_waveplate_ins.mode = 1
@@ -190,22 +200,26 @@ class VariableWaveplateGui(QWidget):
             self.variable_waveplate_ins.set_analog_value(2, Q_(self.voltage_2_textfield.text()))
         elif self.get_mode() == "Modulation":
             self.variable_waveplate_ins.mode = 0
-
-        self.variable_waveplate_ins.freq = Q_(self.frequency_textfield.text())
-
-
+            self.variable_waveplate_ins.freq = Q_(self.frequency_textfield.text())
+        elif self.get_mode() == 'QWP':
+            self.variable_waveplate_ins.quarter_waveplate_voltage(Q_(self.quater_waveplate_textfield.text()) )
 
 
     def set_output_mode(self):
+        """Sets the output on or off deppending on the output_combobox state
+
+        """
+        self.logger.info('Setting the output mode to {}'.format(self.output_combobox.currentText()))
         if self.output_combobox.currentText() == "On":
             self.variable_waveplate_ins.output = True
         elif self.output_combobox.currentText() == "Off":
             self.variable_waveplate_ins.output = False
 
+
 if __name__ == '__main__':
     from hyperion import _logger_format, _logger_settings
 
-    logging.basicConfig(level=logging.INFO, format=_logger_format,
+    logging.basicConfig(level=logging.DEBUG, format=_logger_format,
                         handlers=[
                             logging.handlers.RotatingFileHandler(_logger_settings['filename'],
                                                                  maxBytes=_logger_settings['maxBytes'],
