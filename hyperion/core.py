e2a991bd8fac78ea3ff57955421f4eca8e1c5063
Aron Opheij <a.opheij@tudelft.nl>
2019-12-09 18:30:07 +0100

    Significant changes in logging. In progress

--- hyperion/core.py
+++ hyperion/core.py
@@ -1,5 +1,10 @@
 import os
+import logging
+import logging.handlers
+from time import time
+import datetime
 
+from hyperion import log_path
 
 class Singleton(type):
     """
@@ -12,7 +17,6 @@ class Singleton(type):
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
-
 
 class ANSIcolorFormat:
     """
@@ -155,3 +159,339 @@ class ANSIcolorFormat:
 
 # Already create an object that can be imported if one prefers
 ansicol = ANSIcolorFormat()
+
+# Setting up logging =================================================
+
+class DuplicateFilter(logging.Filter):
+    """ Adding this filter to a logging handler will reduce repeated log-prints"""
+    def filter(self, record):
+        # Note to self. It appears the message from one handler is passed to the next one.
+        # This means that if one handler modifies the message, the next one gets the modified version.
+        self.repeat_message = ' > Logger message is being repeated...'
+        # First strip the repeat_message from the message if it is there:
+        replen = len(self.repeat_message)
+        if len(record.msg) > replen and record.msg[-replen:] == self.repeat_message:
+            record.msg = record.msg[:-replen]
+        # Combine filename, linenumber and (corrected) message to create a record to compare
+        current_record = (record.module, record.lineno, record.msg)
+        # Compare it to the last record stored (if that variable exists)
+        if current_record != getattr(self, "last_record", None):
+            self.last_record = current_record  # store the current record in last_record, to be able to compare it on the next call
+            self.last_unique_time = time()  # store the time of this message
+            self.repeating = False  # set repeating flag to False
+            return True  # Allow this record to be printed
+        else:
+            if not self.repeating:
+                self.repeating = True  # set repeating flag True
+            elif time() > self.last_unique_time + 20:  # if repeting==True AND 20 seconds have passed reset "timer"
+                self.last_unique_time = time()  # reset the last_unique_time to in order repeat occasionally
+            else:
+                return False  # prevent message from being printed during those 20 seconds
+            record.msg += self.repeat_message  # append repeat message
+            return True  # allow the message to be printed
+
+class CustomFormatter(logging.Formatter):
+    """
+    Custom format for log-prints.
+    Adds a lot of information (time, module, line number, method, LEVEL, message)
+    Setting the compact parameter to a value larger than 0 shrinks the date, module and method.
+    Setting it to 1 (maximum) assures the length is maxwidth characters or less.
+    The possible regular color modes are: bright, dim, mixed, bg, universal.
+    The optional secondary color modes for Spyder are: spy_bri, spy_dim, spy_mix, spy_bg, spy_uni.
+    Specifying no color_mode defaults to bright (and spy_bri if Spyder is detected).
+
+    :param compact: float from 0 for full length, to 1 for very compact (defaults to 0)
+    :param maxwidth: integer indicating max line width when compact is 1. None (default) uses 79.
+    :param color: boolean indicating if ANSI colors should be used (defaults to False)
+    :param color_mode: string or tuple/list of two strings (defaults to bright / spy_bri)
+    """
+
+    # 'universal' and 'spy_uni' are chosen to be similar and readable in:
+    # - regular black command window / terminal
+    # - Spyder
+    # - PyCharm default
+    # - PyCharm darcula
+    # - PowerShell (dark blue)
+    # But they're ugly, so I'm using
+
+    # Note: '105;97' is equivalent to ('_m','lw')
+    color_schemes = {
+        'universal': {logging.CRITICAL: ('_m', 'lw'),
+                      logging.ERROR: ('_lr', 'ly'),
+                      logging.WARNING: ('_ly', 'r'),
+                      logging.INFO: ('_lc', 'k'),
+                      logging.DEBUG: ('_lb', 'lw'),
+                      None: None},  # do nothing (for unknown codes)
+        'spy_uni': {logging.CRITICAL: '1;45;37',  # emph, magenta bg, white text
+                    logging.ERROR: '1;41;33',  # emph, red bg, yellow text
+                    logging.WARNING: '1;43;31',  # emph, yellow bg, red text
+                    logging.INFO: '1;46',  # emph, cyan bg, keep text default black
+                    logging.DEBUG: '1;44;37',  # emph, blue bg, white text
+                    None: None},  # do nothing (for unknown codes)
+        'bright': {logging.CRITICAL: 'lm',  # magenta text
+                   logging.ERROR: 'lr',  # redtext
+                   logging.WARNING: 'ly',  # yellowtext
+                   logging.INFO: 'lg',
+                   logging.DEBUG: 'lc',
+                   None: None},  # do nothing (for unknown codes)
+        'dim': {logging.CRITICAL: '35',  # magenta text
+                logging.ERROR: '31',  # redtext
+                logging.WARNING: '33',  # yellowtext
+                logging.INFO: '32',
+                logging.DEBUG: '36',
+                None: None},  # do nothing (for unknown codes)
+        'bg':  {logging.CRITICAL: ('_lm','lw'),
+                logging.ERROR: ('_lr','lw'),
+                logging.WARNING: ('_ly','k'),
+                logging.INFO: ('_lg','k'),
+                logging.DEBUG: ('_lc','k'),
+                None: None},  # do nothing (for unknown codes)
+        'mixed': {logging.CRITICAL: '95',  # magenta text
+                  logging.ERROR: '91',  # redtext
+                  logging.WARNING: '33',  # yellowtext
+                  logging.INFO: '36',
+                  logging.DEBUG: '32',
+                  None: None},  # do nothing (for unknown codes)
+        'spy_bri': {logging.CRITICAL: '1;35',  # magenta text
+                    logging.ERROR: '1;31',  # redtext
+                    logging.WARNING: '1;33',  # yellowtext
+                    logging.INFO: '1;32',
+                    logging.DEBUG: '1;36',
+                    None: None},  # do nothing (for unknown codes)
+        'spy_dim': {logging.CRITICAL: '35',  # magenta text
+                    logging.ERROR: '31',  # redtext
+                    logging.WARNING: '33',  # yellowtext
+                    logging.INFO: '32',
+                    logging.DEBUG: '36',
+                    None: None},  # do nothing (for unknown codes)
+        'spy_bg': {logging.CRITICAL: '1;45',
+                   logging.ERROR: '1;41',
+                   logging.WARNING: '1;43',
+                   logging.INFO: '1;42',
+                   logging.DEBUG: '1;46',
+                   None: None},  # do nothing (for unknown codes)
+    }
+
+    def __init__(self, compact=0.0, maxwidth=None, color=False, color_mode=None):
+        if color_mode is None:
+            self.color_mode = 'bright'  # choose default mode here
+        if any('SPYDER' in name for name in os.environ):
+            if color_mode is not None and len(color_mode) > 1:
+                self.color_mode = color_mode[1]
+            else:
+                self.color_mode = 'spy_bri'  # choose default mode for Spyder here
+
+        if compact > 1:
+            self.compact = 1
+        if compact < 0:
+            self.compact = 0
+        else:
+            self.compact = compact
+        self.color = color
+        self.short_names = {10: 'DEBUG', 20: 'INFO', 30: 'WARN', 40: 'ERROR', 50: 'CRIT'}
+        self.ansicol = ANSIcolorFormat()
+        if maxwidth is None: maxwidth = 79                      # default value goes here
+        self.maxwidth = int(maxwidth if maxwidth>56 else 56)    # shorter then 53 might cause errors
+        super(CustomFormatter, self).__init__()
+
+    def format(self, record):
+        module = record.name
+        func = record.funcName
+        if self.compact<0.5:
+            lvl_str = '{:>8} '.format(record.levelname)
+        else:
+            lvl_str = '{:>5}'.format(self.short_names[record.levelno])
+        if self.color:
+            if record.levelno not in self.color_schemes[self.color_mode]:
+                lvl_str = ansicol(lvl_str, self.color_schemes[self.color_mode][None])
+            else:
+                lvl_str = ansicol(lvl_str, self.color_schemes[self.color_mode][record.levelno])
+
+        #        colored_levelname = (COLOR+'{:>8} '+RESET).format('51', record.levelname)
+        #        colored_levelname = '{:>8} '.format(record.levelname)
+        if self.compact == 0:
+            timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[
+                        :-3]  # show 3 out of 6 digits (i.e. milliseconds)
+            # module = module[-min(len(module), 50):]       # truncate from the left to 50 characters
+            # msg = '{:.200}'.format(record.msg)            # truncate to 200 characters
+            # if len(func)>30:
+            #     func = func[:27]+'...'
+            message = '{} |{:>50} |{:>5} | {:32}|{}| {}'.format(timestamp, module, record.lineno, func + '()', lvl_str,
+                                                                record.msg)
+        else:
+            timestamp = datetime.datetime.now().strftime('%H:%M:%S')
+            # module = '.'.join(module.split('.')[1:])  # strip off the first word before '.'(i.e. hyperion)
+            lmod = int(50 - 36 * self.compact)  # >=14
+            lfun = int(32 - 20 * self.compact)  # >=12
+            if len(module) > lmod:
+                module = '...' + module[-(lmod - 3):]  # truncate from the left to 38 characters
+            #                msg = '{:.30}'.format(record.msg)           # truncate to 30 characters
+            if len(func) > lfun:
+                func = func[:(lfun - 3)] + '...'
+
+            if self.compact < 0.5:
+                message = '{} |{:>{mod_w}} |{:>5} | {:{fun_w}}|{}| {}'.format(timestamp, module, record.lineno,func + '()',
+                                                                              lvl_str, record.msg, mod_w=lmod, fun_w=lfun+2)
+            else:
+                if record.lineno > 9999:
+                    linenr = '>10k'
+                else:
+                    linenr = record.lineno
+                tmpmsg = record.msg.replace('\n',' ')
+
+                if self.compact==1 and len(tmpmsg) > (self.maxwidth-50):   # for length of 79
+                    msg = tmpmsg[:(self.maxwidth-53)]+'...'
+                else:
+                    msg = record.msg
+                message = '{} {:>{mod_w}} {:>4} {:{fun_w}} {} {}'.format(timestamp, module, linenr, func + '()',
+                                                                          lvl_str, msg, mod_w=lmod, fun_w=lfun+2)
+
+        return message
+
+class LoggingManager(metaclass=Singleton):
+    CRITICAL = logging.CRITICAL
+    ERROR = logging.ERROR
+    WARNING = logging.WARNING
+    WARN = logging.WARN
+    INFO = logging.INFO
+    DEBUG = logging.DEBUG
+    _level_2_number = {'CRITICAL': CRITICAL, 'ERROR': ERROR, 'WARNING': WARNING, 'WARN': WARN, 'INFO': INFO,
+                       'DEBUG': DEBUG}
+    _number_2_level = {CRITICAL: 'CRITICAL', ERROR: 'ERROR', WARNING: 'WARNING', INFO: 'INFO', DEBUG: 'DEBUG'}
+
+    def __init__(self, name=None, default_path, default_name):
+        self._default_path = default_path
+        self._default_name = default_name
+        self.enable_stream = True
+        self.enable_file = True
+        
+        #        if name is None:
+        #            name = __name__
+        #        super().__init__(name)
+
+        #        self._logger_format_long  = '%(asctime)s |%(name)+50s | %(funcName)+30s() |%(levelname)+7s | %(message)s'
+        #        self._logger_format_short = '%(asctime)s |%(module)+22s | %(funcName)+22s()|%(levelname)+7s | %(message).40s'
+        # _logger_format_short = '%(asctime)s.%(msecs).3d |%(module)+22s | %(funcName)+22s()|%(levelname)+7s | %(message).40s'
+        # note: by adding ,'%H:%M:%S' to the Formatter (asctime) will turn into HH:MM:SS
+
+
+
+        if pathname is not None:
+            self._log_path = os.path.dirname(pathname)
+            self._fname = os.path.base(pathname)
+        else:
+            self._log_path = log_path
+            self._fname = 'hyperion.log'
+
+
+
+        # create handler for file logging:
+        self._default_log_filename = os.path.join(self._log_path, fname)
+
+
+    def set_stream_handler(self, color=True, compact=0.5, reduce_duplicates=True, maxwidth=None, color_mode=None, **kwargs):
+        self.enable_stream = True
+        self.stream_handler = logging.StreamHandler(**kwargs)
+        self.stream_handler.setFormatter(CustomFormatter(compact=compact, color=color, color_mode=color_mode, maxwidth=maxwidth))
+        self.stream_handler.setLevel(self.DEBUG)  # default level for stream handler
+        if reduce_duplicates:
+            self.stream_handler.addFilter(DuplicateFilter())
+
+    def set_file_handler(self, pathname=None, compact=0, reduce_duplicates=True, maxwidth=None, maxBytes=(5 * 1024 * 1024), backupCount=9, **kwargs):
+        """
+        Sets (overwrites) the file handler.
+
+        :param pathname: False removes the file handler
+        :param compact: see CustomFormatter (defaults to 0)
+        :param reduce_duplicates: (bool) (defaults to True)
+        :param maxBytes: see logging.handlers.RotatingFileHandler() (defaults to 5 * 1024 * 1024)
+        :param backupCount: see logging.handlers.RotatingFileHandler() (defaults to 9)
+        :param **kwargs: additional keyword arguements are passed into logging.handlers.RotatingFileHandler()
+        """
+        self.enable_file = True
+        if pathname is None:
+            log_path = self._log_path
+            log_name = self._log_name
+
+
+        self.file_handler = logging.handlers.RotatingFileHandler(filename=pathname, maxBytes=maxBytes,
+                                                                 backupCount=backupCount, **kwargs)
+        self.file_handler.setFormatter(CustomFormatter(compact=compact, maxwidth=maxwidth))
+        self.file_handler.setLevel(self.DEBUG)  # default level for file handler
+        if reduce_duplicates:
+            self.file_handler.addFilter(DuplicateFilter())
+
+    @property
+    def stream_level(self):
+        return self._number_2_level[self.stream_handler.level]
+
+    @stream_level.setter
+    def stream_level(self, number_or_string):
+        self.stream_handler.setLevel(number_or_string)
+
+    @property
+    def file_level(self):
+        return self._number_2_level[self.file_handler.level]
+
+    @file_level.setter
+    def file_level(self, number_or_string):
+        self.file_handler.setLevel(number_or_string)
+
+    #    @property
+    #    def stream_enable(self):
+    #        return self.stream_handler in self.handlers
+    #
+    #    @stream_enable.setter
+    #    def stream_enable(self, boolean):
+    #        self._enable(boolean, self.stream_handler)
+    #
+    #    @property
+    #    def file_enable(self):
+    #        return self.file_handler in self.handlers
+    #
+    #    @file_enable.setter
+    #    def file_enable(self, boolean):
+    #        self._enable(boolean, self.file_handler)
+
+    def _enable(self, boolean, handler):
+        # Helper function
+        state = handler in self.handlers
+        if boolean is not state:
+            if boolean:
+                self.addHandler(handler)
+            else:
+                self.removeHandler(handler)
+
+    def getLogger(self, name, add_stream=None, add_file=None):
+        """
+        Returns logger object.
+
+        :param name: (str) Name, usually the module name. i.e. __name__
+        :param add_stream: (bool or None) add the streamhandler. None (default) uses object.enalbe_stream
+        :param add_file: (bool or None) specifying if the filehandler should be added (default True)
+        :return: logger object
+        """
+        if name is None:
+            name = __name__
+
+        logger = logging.getLogger(name)
+        logger.setLevel(logging.DEBUG)      # this is necessary for some reason
+        logger.handlers = []                # to avoid duplicate handlers, remove all existing
+        # def remove_stream_handler(self):
+        #     for i,h in enumerate(self.handlers):
+        #         if type(h) is logging.StreamHandler:
+        #             del self.handlers[i]
+        # def remove_file_handler(self):
+        #     for i,h in enumerate(self.handlers):
+        #         if type(h) is logging.handlers.RotatingFileHandler:
+        #             del self.handlers[i]
+        # setattr(logger, 'remove_stream_handler', remove_stream_handler)
+        # setattr(logger, 'remove_file_handler', remove_file_handler)
+        if add_stream or (add_stream is None and self.enable_stream):
+            logger.addHandler(self.stream_handler)
+        if add_file or (add_file is None and self.enable_file):
+            logger.addHandler(self.file_handler)
+        return logger
+
+# Initialize logger object. Import this in other modules inside this package.
+log = LoggingManager()
