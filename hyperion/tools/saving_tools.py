75ec9268ea88bcc0924bbfb07bea5925d7f6a57d
Martin Caldarola <m.caldarola@tudelft.nl>
2020-01-02 17:22:51 +0100

    added xarray to the environment and added the netcdf4 saving function.

--- hyperion/tools/saving_tools.py
+++ hyperion/tools/saving_tools.py
@@ -4,15 +4,21 @@
 Saving tools
 ============
 
-This is a collection of useful methods used along hyperion.
+This is a collection of useful methods related to saving data and metadata
+used along hyperion.
 
-NOT DONE YET
+
 
 """
-from hyperion import root_dir, ur
 import os
-import sys
-from hyperion import logging
+import netCDF4
+import time
+import matplotlib.pyplot as plt
+import xarray as xr
+from hyperion.core import logman
+from hyperion import __version__, ur, Q_
+
+logger = logman.getLogger(__name__)
 
 
 def name_incrementer(basename, list_of_existing, separator='_', fill_zeros=0, use_number_for_first=None,
@@ -80,7 +86,6 @@ def name_incrementer(basename, list_of_existing, separator='_', fill_zeros=0, us
 
     return basename + ext
 
-
 def create_filename(file_path):
     """ Creates the filename, so all the methods point to the same folder
     and save with the same name. The output does not include the extension but the input does.
@@ -122,6 +127,168 @@ def save_metadata(self):
 
     self.logger.info('Metadata saved to {}'.format(file_path))
 
-
 def save(self, data):
     pass
+
+def save_netCDF4(filename, detectors, data, axes, axes_name, extra_dims=None, description=None):
+    """ This function saves the data in a netCDF4 format, including units and
+    the axes corresponding to the data. For more info about the package used,
+    please see http://unidata.github.io/netcdf4-python/netCDF4/index.html.
+    For info on the format itself, refer to: https://www.unidata.ucar.edu/software/netcdf/docs/index.html
+
+
+    :param detectors: list of the detectors used
+    :type detectors: list of strings
+    :param data: list with the actual data corresponding to each detector
+    :type data: list of numpy ndarrays of pint quantities
+    :param axes: corresponding coordinates for the data. the dimension of the i element
+    in the list has to match the dimension of the i-th dimension of data.
+    :type axes: list of vectors of pint quantity
+    :param axes_name: the name of each of the axes
+    :type axes_name: string
+    :param extra_dims: A dict containing extra values fixed and all the same for the set.
+    :type extra_dims: dict
+    :param description: an extra descriptive message can be put here.
+    :type description: string
+
+    """
+    logger.debug('Checking the input')
+    assert len(data) == len(detectors)
+    assert len(axes) == len(axes_name)
+
+    rootgrp = netCDF4.Dataset(filename, "w", format="NETCDF4")
+    dims = {}
+    dim_vars = {}
+
+    logger.debug('Creating dimensions.')
+    # create the dimensions
+    for i, (ax, name) in enumerate(zip(axes, axes_name)):
+        dim = rootgrp.createDimension(name, len(ax))
+        dim_var = rootgrp.createVariable(name, 'f8', (name,))
+        dim_var[:] = ax.m
+        dim_var.units = '{}'.format(ax.u)
+
+        dims[name] = dim
+        dim_vars[name] = dim_var
+
+    # logger.debug('Creating extra dimensions.')
+    # # create extra variables and dimensions for other parameters,
+    # # fixed in the dataset
+    # ex_dims = {}
+    # ex_dim_vars = {}
+    # if extra_dim is not None:
+    #     for name, value in extra_dim:
+    #         logger.debug('Adding coordinate: {}, with value: {}'.format(name, value))
+    #         dim = rootgrp.createDimension(name, 1)
+    #         dim_var = rootgrp.createVariable(name, 'f8', (name,))
+    #         if isinstance(value, Q_):
+    #             dim_var[:] = value.m
+    #             dim_var.units = '{}'.format(value.u)
+    #         else:
+    #             dim_var[:] = value
+    #
+    #         ex_dims[name] = dim
+    #         ex_dim_vars[name] = dim_var
+
+    # fill the data
+    logger.debug('Saving the data.')
+    for detector, data in zip(detectors, data):
+        u = data.u
+        var = rootgrp.createVariable(detector, data.m.dtype, tuple(dims.keys())[::1])
+        var[:] = data.m
+        var.units = '{}'.format(u)
+
+    # metadata
+    logger.debug('Creating metadata.')
+
+    for k, v in extra_dims.items():
+        logger.debug('Adding extra dimension: {} with value {}'.format(k, v))
+        s = '{!s}'.format(v)
+        #logger.debug('The value as a type = {} is {}'.format(type(s), s))
+
+        if not isinstance(s, (str, float, int)):
+            logger.warning("not saving extra dimension {} = {}".format(k, repr(s)))
+            continue
+
+        setattr(rootgrp, k, s)
+
+    if description is None:
+        rootgrp.description = 'No description given'
+    else:
+        rootgrp.description = description
+
+    rootgrp.history = "Saved on {}".format(time.ctime(time.time()))
+    rootgrp.creator = f'NanoCD package {__version__}'
+    # save
+    rootgrp.sync()
+    rootgrp.close()
+
+def read_netcdf4_and_plot(filename):
+    """Reads the file in filename and plots all the detectors """
+    ds = xr.open_dataset(filename)
+    logger.info('The dataset contains: {}'.format(ds))
+
+    for index, name in enumerate(ds.data_vars):
+        plt.figure(figsize=((12, 6)))
+        ds[name].plot()
+        plt.axes.set_axis = 'equal'
+        plt.tight_layout()
+
+    return ds
+
+if __name__ == '__main__':
+    import numpy as np
+
+    # uncomment the next line according to the test you would like to run.
+    # m = 'write and read'
+    m = 'read'
+
+    # if to decide which mode to test
+    if m == 'read':
+        ## read
+        filename = 'c:/Users/Martin/Desktop/test_netcfd4_output_from_experiment.nc'
+        logger.info(f'Now reading the data in file: {filename} ')
+        # %% read the data
+        ds = read_netcdf4_and_plot(filename)
+
+    elif m == 'write and read':
+        # write
+        filename = 'test_netcfd4_output.nc'
+        logger.info('Filename to save and read: {}'.format(filename))
+
+        # fake dataset
+        logger.info('Create a fake dataset to test the saving functions.')
+        wavelength = 551 * ur('nm')
+        extra_dim = {'wavelength': wavelength, 'temp': 300*ur('K'), 'points to average': 1}
+        size = (332, 150)
+        axes = []
+        axes_name = []
+
+        for index, s in enumerate(size):
+            axes.append(np.linspace(0, 1, s) * ur('um'))
+            axes_name.append('x{}'.format(index + 1))
+
+        detectors = ['Photothermal', 'APD']
+        units = ['V', 'cps']
+
+        data_matrix = np.random.random(size)
+        data = [data_matrix * ur(units[0]), data_matrix * 1e3 * ur(units[1])]
+        logger.debug('The data is: {}'.format(data))
+
+            # data description
+        data_description = []
+        for i in range(len(data)):
+            data_description.append('description for data {}'.format(i+1))
+        logger.debug('The data description is: {}'.format(data_description))
+
+        # save
+        logger.info('Now saving the data...')
+        save_netCDF4(filename, detectors, data, axes, axes_name,
+                     extra_dims = extra_dim,
+                     description='This is fake data to test the saving process')
+
+        # read the data
+        logger.info('Now reading the data... ')
+        ds = read_netcdf4_and_plot(filename)
+
+
