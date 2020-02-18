f64b29ed54b669e44a6793a2f886a7e87b3295e4
Marc Noordam <m.l.noordam@tudelft.nl>
2020-02-18 13:20:09 +0100

    Added a PI linearstage controller and instrument classes

---
+++ hyperion/instrument/position/pi_motor.py
@@ -0,0 +1,68 @@
+"""
+====================================
+Thorlabs thorlabs_motor Instrument
+====================================
+
+Connects to the pi_motorcontroller controller, which is just a wrapper for the core underneath that we installed.
+
+"""
+from hyperion import logging
+from hyperion.instrument.base_instrument import BaseInstrument
+from hyperion import ur
+import time
+
+class PImotor(BaseInstrument):
+    """ Thorlabsmotor instrument
+    """
+    def __init__(self, settings):
+        """ init of the class"""
+
+        super().__init__(settings)
+        self.logger = logging.getLogger(__name__)
+
+        # properties
+        self._output = False
+        self._mode = 0
+        self.logger.info('Initializing PI motor settings: {}'.format(settings))
+
+        self.settings = settings
+        self._name = self.settings['name']
+
+        self.initialize()
+
+
+
+    def initialize(self):
+
+        self.logger.info('Initializing your {} and checking some basic things.'.format(self._name))
+
+        self.real_controller=self.controller.initialize()
+
+
+
+    def finalize(self):
+        """ This would have been to close connection to the device, but that method does not exist in the core controller.
+        """
+        self.logger.info('Should close connection to cube: {}'.format(self._name))
+
+    def position(self):
+        """| Asks the position to the controller and returns that.
+
+        """
+        pos = self.real_controller.qPOS()
+        position=float(pos['A'])
+        return position
+
+    def move_absolute(self,target):
+        self.real_controller.MOV(self.real_controller.axes, target)#in mm
+
+    def move_relative(self,shift):
+        target=self.position()+shift
+        self.real_controller.MOV(self.real_controller.axes, target)
+
+if __name__ == "__main__":
+
+    settings = {'controller': 'hyperion.controller.pi.pi_motorcontroller/PI_motorcontroller', 'name': 'Delaystage775','controllername': "C-836.10",'stages':'M-404.6PD','refmode':'MNL'}
+
+    with PImotor(settings = settings) as sampleX:
+        sampleX.position()
